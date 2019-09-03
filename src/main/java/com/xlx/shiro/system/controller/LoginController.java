319d6d7834254cd9cbcc5bdc744591eb37f7675a
leetcoder <perfectxlx@gmail.com>
2019-09-04 00:28:15 +0800

    change UnAuthenticationException to UnKnownAccountException

--- src/main/java/com/xlx/shiro/system/controller/LoginController.java
+++ src/main/java/com/xlx/shiro/system/controller/LoginController.java
@@ -6,17 +6,15 @@ import com.xlx.shiro.system.dto.LoginDTO;
 import com.xlx.shiro.system.dto.ResultDTO;
 import com.xlx.shiro.system.service.UserService;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.ExcessiveAttemptsException;
-import org.apache.shiro.authc.LockedAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authz.UnauthenticatedException;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 /**
@@ -27,7 +25,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class LoginController {
 
-	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
 	@Autowired
 	private UserService userService;
@@ -52,13 +50,13 @@ public class LoginController {
 			this.userService.recordLoginTime(loginDTO.getUsername());
 			logger.info( "User [" + loginDTO.getUsername() + "] logged in successfully." );
 			return ResultDTO.success("登录成功");
-		}catch (LockedAccountException | ExcessiveAttemptsException | UnauthenticatedException e){
+		}catch (UnknownAccountException | ExcessiveAttemptsException | LockedAccountException e){
 			//帐号锁定/连续试错5次/用户名或密码错误
 			logger.error("there is a mistake happened:[{}]",e.getMessage());
 			return ResultDTO.failed(e.getMessage());
 		}catch (AuthenticationException e){
 			logger.error("occurred an AuthenticationException:[{}]",e.getMessage());
-			return ResultDTO.failed("登录失败");
+			return ResultDTO.failed("用户名或密码错误");
 		}
 
 	}
--- src/main/java/com/xlx/shiro/system/shiro/realm/UserRealm.java
+++ src/main/java/com/xlx/shiro/system/shiro/realm/UserRealm.java
@@ -1,15 +1,12 @@
 package com.xlx.shiro.system.shiro.realm;
 
-import com.xlx.shiro.system.entity.Role;
 import com.xlx.shiro.system.entity.User;
 import com.xlx.shiro.system.service.MenuService;
 import com.xlx.shiro.system.service.RoleService;
 import com.xlx.shiro.system.service.UserService;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
-import org.apache.shiro.authz.UnauthenticatedException;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
@@ -17,12 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.annotation.Resource;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * 自定义的Realm
@@ -30,67 +22,68 @@ import java.util.stream.Collectors;
  * @author xielx on 2019/7/13
  */
 public class UserRealm extends AuthorizingRealm {
-
-  private static final Logger logger = LoggerFactory.getLogger(UserRealm.class);
-
-  @Autowired
-  private UserService userService;
-
-  @Autowired
-  private RoleService roleService;
-
-  @Autowired
-  private MenuService menuService;
-  /**
-   * 权限认证
-   */
-  @Override
-  protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-    logger.info("===================权限认证===============");
-    User user = (User) principals.getPrimaryPrincipal();
-    String userName = user.getUserName();
-
-    SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
-
-    // 获取用户角色集
-    Set<String> roleSet = roleService.findRoleKey(userName);
-    logger.info("USER[{}]的角色集:[{}]",userName,roleSet);
-    authorizationInfo.setRoles(roleSet);
-
-    // 获取用户权限集
-    Set<String> permissionSet = menuService.findPermissions(userName);
-    logger.info("USER[{}]的权限集:[{}]",userName,permissionSet);
-    authorizationInfo.setStringPermissions(permissionSet);
-
-    return authorizationInfo;
-  }
-
-  /**
-   * 身份认证
-   */
-  @Override
-  protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-    logger.info("==============身份认证==================");
-    UsernamePasswordToken upToken = (UsernamePasswordToken) token;
-    String username = upToken.getUsername();
-    logger.info("username:{}",username);
-    User user = userService.findUserByUserName(username);
-    logger.info("user:[{}]",user);
-    if (user == null) {
-      logger.error("occurred an UnauthenticatedException: account number or credential is wrong");
-      throw new UnauthenticatedException("帐号或密码错误");
-    }else if (Boolean.TRUE.equals(user.getLocked())) {
-      logger.error("occurred an UnauthenticatedException: this account has been locked,please contact the administrator");
-      throw new LockedAccountException("帐号被锁定,请联系管理员");
-    }
-
-
-    // 交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配,也可自定义
-    SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(user,
-            user.getUserPassword(),
-            ByteSource.Util.bytes(user.getCredentialsSalt()),
-            getName());
-
-    return simpleAuthenticationInfo;
-  }
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserRealm.class);
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private RoleService roleService;
+	
+	@Autowired
+	private MenuService menuService;
+	
+	/**
+	 * 权限认证
+	 */
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		logger.info("===================权限认证===============");
+		User user = (User) principals.getPrimaryPrincipal();
+		String userName = user.getUserName();
+		
+		SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+		
+		// 获取用户角色集
+		Set<String> roleSet = roleService.findRoleKey(userName);
+		logger.info("USER[{}]的角色集:[{}]", userName, roleSet);
+		authorizationInfo.setRoles(roleSet);
+		
+		// 获取用户权限集
+		Set<String> permissionSet = menuService.findPermissions(userName);
+		logger.info("USER[{}]的权限集:[{}]", userName, permissionSet);
+		authorizationInfo.setStringPermissions(permissionSet);
+		
+		return authorizationInfo;
+	}
+	
+	/**
+	 * 身份认证
+	 */
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		logger.info("==============身份认证==================");
+		UsernamePasswordToken upToken = (UsernamePasswordToken) token;
+		String username = upToken.getUsername();
+		logger.info("username:{}", username);
+		User user = userService.findUserByUserName(username);
+		logger.info("user:[{}]", user);
+		if (user == null) {
+			logger.error("occurred an UnauthenticatedException: the account number does not exist");
+			throw new UnknownAccountException("帐号或密码错误");
+		} else if (Boolean.TRUE.equals(user.getLocked())) {
+			logger.error("occurred an UnauthenticatedException: this account has been locked,please contact the administrator");
+			throw new LockedAccountException("帐号被锁定,请联系管理员");
+		}
+		
+		
+		// 交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配,也可自定义
+		SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(user,
+						user.getUserPassword(),
+						ByteSource.Util.bytes(user.getCredentialsSalt()),
+						getName());
+		
+		return simpleAuthenticationInfo;
+	}
 }
