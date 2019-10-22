846158696bd18a40ce7dde68403df78c131bc166
perfectxlx <perfectxlx@gmail.com>
2019-10-22 18:48:55 +0800

    注解类测试

--- src/main/java/com/xlx/shiro/common/annotation/ExportConfig.java
+++ src/main/java/com/xlx/shiro/common/annotation/ExportConfig.java
@@ -47,4 +47,7 @@ public @interface ExportConfig {
      * @return String
      `*/
     String replace() default "";
+    
+    
+    
 }
--- src/test/java/com/xlx/shiro/annotationdemo/AnnotationTest.java
+++ src/test/java/com/xlx/shiro/annotationdemo/AnnotationTest.java
@@ -1,6 +1,11 @@
 package com.xlx.shiro.annotationdemo;
 
+import com.xlx.shiro.common.annotation.ExportConfig;
 import com.xlx.shiro.system.entity.User;
+import org.junit.Test;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 
 /**
  * 注解测试
@@ -10,9 +15,13 @@ import com.xlx.shiro.system.entity.User;
 public class AnnotationTest {
     
     
-    public void testUserAnnotation(){
+    @Test
+    public void testUserAnnotation() {
     
         Class<User> userClass = User.class;
-        userClass.getAnnotations();
+        /*Field field = userClass.getDeclaredField("userName");
+        ExportConfig exportConfig = field.getAnnotation(ExportConfig.class);*/
+    
+        boolean present = userClass.isAnnotationPresent(ExportConfig.class);
     }
 }
